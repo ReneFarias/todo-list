c30b06874ac9736bb9522a0a650b20b8101fd9db
ReneFarias <renefariasfeitosadasilva@gmail.com>
2023-02-11 00:23:38 -0300

    first commit

---
+++ src/components/EditTodoDialog.jsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
+import { TextField } from '@mui/material';
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>,
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function EditTodoDialog({open, dialogHandler, todo, editTodo }) {
+  const [editText, setEditText] = React.useState(todo.text);
+
+  const textHandler = () => {
+editTodo(todo.id, editText);
+dialogHandler();
+  }
+
+  return (
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={dialogHandler}
+        aria-describedby="alert-dialog-slide-description"
+        fullWidth
+      >
+        <DialogTitle>{"Editando TAREFA"}</DialogTitle>
+        <DialogContent>
+          <TextField defaultValue={editText} fullWidth onChange={ (e)=>setEditText(e.target.value)}/>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={dialogHandler}>Cancelar</Button>
+          <Button onClick={textHandler}>Ok</Button>
+        </DialogActions>
+      </Dialog>
+  );
+}
